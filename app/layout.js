28f539af468d47a0790e1a9a11c55771920af805
aakash-sharma25 <as024harma@gmail.com>
2024-11-06 15:42:35 +0530

    added files

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import logo from "@/public/logo.jpg";
-import Head from "next/head";
+import Navbar from "./components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,12 +22,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href={logo} sizes="any" />
-      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>

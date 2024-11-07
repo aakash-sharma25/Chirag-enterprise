58c9ded4dafccc02145559021f73e247ff25e292
aakash-sharma25 <as024harma@gmail.com>
2024-11-07 11:20:44 +0530

    changed layout

--- app/page.js
+++ app/page.js
@@ -1,13 +1,19 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Services from "./components/Services";
+import Getquote from "./components/Getquote";
+import Strip from "./components/Strip";
 
 export default function Home() {
   return (
     <>
-      
+      <div className=" mt-[50px]"/>
       <Hero />
       <About />
+      <Strip/>
+      <Services/>
+      <Getquote/>
     </>
   );
 }

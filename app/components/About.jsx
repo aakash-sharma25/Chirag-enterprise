c02f420efbc35a163e1ce4bd9c56a24ea78389aa
aakash-sharma25 <as024harma@gmail.com>
2024-11-27 11:39:41 +0530

    added slideshow

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -15,7 +15,7 @@ export default function About() {
           height="400"
           className="rounded-md  shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]"
         >
-          <source src="/intro.mp4" type="video/mp4" />
+          <source src="/slideshow.mp4" type="video/mp4" />
           Your browser doesn't support the video tag.
         </video>
 

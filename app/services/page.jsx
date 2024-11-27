89ee0d5ba566291a38f63d9f2b618319331467e8
aakash-sharma25 <as024harma@gmail.com>
2024-11-27 11:05:42 +0530

    updated code

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -15,20 +15,20 @@ import { data } from "../data";
 export default function page() {
   return (
     <>
-      <Services />
-      <div className="flex justify-evenly flex-wrap gap-10 max-w-screen-lg mx-auto">
-        <ServiceCard img={cotajoining} name={"Cota Joniding Flooring"} />
-        <ServiceCard img={antyiscert} name={"Anti Scert Flooring"} />
-        <ServiceCard img={pucoting} name={"PU Coting Flooring"} />
-        <ServiceCard img={sdflooring} name={"SD Flooring"} />
-        <ServiceCard img={yellowmarking} name={"Yellow Marking"} />
-      </div>
       <div className="flex justify-evenly flex-wrap gap-10 max-w-screen-lg mx-auto my-10">
         <ServiceCard img={onemm} name={"1 MM"} />
         <ServiceCard img={twomm} name={"2 MM"} />
         <ServiceCard img={threemm} name={"3 MM"} />
         <ServiceCard img={fourmm} name={"4 MM"} />
-        {data["antiSkidFlooring"]}
+        {/* {data["antiSkidFlooring"]} */}
+      </div>
+      <Services />
+      <div className="flex justify-evenly flex-wrap gap-10 max-w-screen-lg mx-auto">
+        {/* <ServiceCard img={cotajoining} name={"Cota Joniding Flooring"} /> */}
+        <ServiceCard img={antyiscert} name={"Anti Scert Flooring"} />
+        <ServiceCard img={pucoting} name={"PU Coting Flooring"} />
+        <ServiceCard img={sdflooring} name={"SD Flooring"} />
+        <ServiceCard img={yellowmarking} name={"Yellow Marking"} />
       </div>
     </>
   );

3b7ace98a8812a4729a7f6f19da23f6d83d9c044
aakash-sharma25 <as024harma@gmail.com>
2024-11-30 15:56:15 +0530

    added data in pages

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Services from "../components/Services";
 import ServiceCard from "../components/ServiceCard";
 import antyiscert from "@/public/antiyscert.jpg";
-import cotajoining from "@/public/cotajoniding.jpg";
 import pucoting from "@/public/pucoting.jpg";
 import sdflooring from "@/public/sdflooring.jpg";
 import yellowmarking from "@/public/yellowmarking.jpg";
@@ -90,10 +89,10 @@ export default function page() {
       </div>
       <Services />
       <div className="flex justify-evenly flex-wrap gap-10 max-w-screen-lg mx-auto mb-10">
-        <ServiceCard img={antyiscert} name={"Anti Scert Flooring"} />
-        <ServiceCard img={pucoting} name={"PU Coting Flooring"} />
-        <ServiceCard img={sdflooring} name={"SD Flooring"} />
-        <ServiceCard img={yellowmarking} name={"Yellow Marking"} />
+        <ServiceCard img={antyiscert} name={"Anti Skid Flooring"} id="anti-skid-flooring" />
+        <ServiceCard img={pucoting} name={"PU Coting Flooring"} id="pu-coating-flooring" />
+        <ServiceCard img={sdflooring} name={"SD Flooring"} id="sd-flooring" />
+        <ServiceCard img={yellowmarking} name={"Yellow Marking"} id="yellow-marking" />
       </div>
     </>
   );
